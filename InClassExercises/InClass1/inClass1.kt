9df490433693d89c28cc4ce9758abb850c6a25dc
James Anderson <james.anderson@wright.edu>
2025-01-17 11:10:36 -0500

    couple edits to inClass1.kt

--- InClassExercises/InClass1/inClass1.kt
+++ InClassExercises/InClass1/inClass1.kt
@@ -48,13 +48,12 @@ fun main() {
     // of cats and dogs using the variables, like:
     // There are 2 cats and 4 dogs
 
-    // 7. Write a when statement to make a string val or var and have it say
-    // whether there are more cats than dogs, more dogs than cats, are there
-    // are the same number of each
+    // 7. Write a when statement that prints whether there are more cats than 
+    // dogs, more dogs than cats, are there are the same number of each.
 
     // 8. Use when to determine if a character is a vowel or consonant and
     // print the result. Char has methods named lowerCaseChar() and
-    // upperCaseChar() you can use
+    // upperCaseChar() you can use, or you can assume upper/lowercase
 
     // 9. Use a for loop to print the numbers 0 to 9 in reverse order. Hint -
     // there is a downTo keyword
@@ -68,16 +67,16 @@ fun main() {
 
 
     // 12. Remove your most and least favorite from the combined list, and print
-    // this new list
+    // this new list. (You may need to modify what you did for 10 and 11)
 
 
     // 13. Create a string variable and set it to null, then print the string
 
-    // 14. Make a variable and set it to the length of the string such that if
-    // the string from 13. is null, the variable will be -1, then print the length
+    // 14. Make a variable and set it to the length of the string from 13 such that if
+    // the string is null, the variable will be -1, then print the length.
 
     // 15. Write a when statement that returns a string saying if the string
     // is empty, the string is short, the string is medium length, or the string
-    // is long. It's up to you as to what short, medium, and long strings are
+    // is long. It's up to you as to what short, medium, and long strings are.
 
 }
