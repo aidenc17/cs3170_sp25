a4bb8c2d785dbd0cf4406fb9d6698202cec79997
James Anderson <james.anderson@wright.edu>
2025-01-29 14:29:16 -0500

    added an image to the birthday card project

--- android_examples/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ android_examples/HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -12,8 +15,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -27,6 +33,12 @@ class MainActivity : ComponentActivity() {
             HappyBirthdayTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                 // you might have red squiggles under innerPadding, it shouldn't be an issue
+                    GreetingText(
+                        message = "Happy Birthday Paris!",
+                        from = "From James",
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    )
                 }
             }
         }
@@ -42,34 +54,70 @@ fun GreetingText(
     from: String,
     modifier: Modifier = Modifier
 ) {
+
     // Column places elements vertically
     // the things we want to be in the column go in between
     // the curly braces right after the parentheses
     // since Column is the parent element, we pass the modifier on to it
-    Column(modifier = modifier)
+    Column(
+        verticalArrangement = Arrangement.Center, // things will be centered within the Column
+                                                    // this does not specify where the Column goes in relation to its parent
+        modifier = modifier.padding(8.dp))
     {// stuff in here will be drawn in the column
         // text to display the happy birthday message
         Text(
             text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp, // without line height, the lines are jumbled together
+            textAlign = TextAlign.Center // textAlign aligns the text within its box
         )
         // text to display the from String
         Text(
             text = from,
-            fontSize = 36.sp
+            fontSize = 36.sp,
+            //textAlign = TextAlign.Right,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End) // this align moves the text box to the End (Right)
+
         )
     }
 
+}
+
+@Composable
+fun GreetingImage(
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier,
+) {
+    // R gives us access to resources
+    // painter resource takes the ID of a drawable
+    val image = painterResource(R.drawable.img_0035)
+
+    Box(modifier = modifier)
+    { // Box lets us place items on top of each other, and we can control how they are arranged
+        Image(
+            painter = image,
+            // contentDescription is required for images for accessibility purposes
+            // for assignments you can pass null, or give a string if you wish
+            contentDescription = "A dog"
+        )
+
+        GreetingText(
+            message = message,
+            from = from,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(
+        GreetingImage(
             message = "Happy Birthday James!",
-            from = "From Paris"
+            from = "From Paris",
         )
     }
 }
