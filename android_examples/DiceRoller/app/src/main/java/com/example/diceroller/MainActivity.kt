e495637e4595a11c4976d4fd38bc1b4858d70224
James Anderson <james.anderson@wright.edu>
2025-02-12 13:00:25 -0500

    finished dice roller

--- android_examples/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ android_examples/DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -12,8 +13,13 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,8 +35,10 @@ class MainActivity : ComponentActivity() {
             DiceRollerTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     DiceRollerApp(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                            .wrapContentSize(Alignment.Center))
                 }
             }
         }
@@ -39,11 +47,25 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceRollerApp(modifier: Modifier = Modifier) {
+    var result by remember {mutableStateOf(1)}
+    val imageSource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        6 -> R.drawable.dice_6
+        else -> R.drawable.dice_6
+    }
     Column(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-        Button(onClick = {}){
+        Image(
+            painter = painterResource(id = imageSource),
+            contentDescription = result.toString()
+        )
+        Button(onClick = {result = (1..6).random()}){
             Text("Roll", fontSize = 40.sp)
         }
     }
@@ -51,7 +73,7 @@ fun DiceRollerApp(modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun DiceRollerPreview() {
     DiceRollerTheme {
         DiceRollerApp(modifier = Modifier
             .fillMaxSize()
