58c403aff4a81ca5226c8adea11c051ca7c172ec
James Anderson <james.anderson@wright.edu>
2025-03-14 14:44:44 -0400

    have list working, next will will customize the theme

--- android_examples/SuperHeroes/app/src/main/java/com/example/superheroes/MainActivity.kt
+++ android_examples/SuperHeroes/app/src/main/java/com/example/superheroes/MainActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Scaffold
@@ -18,9 +26,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.superheroes.data.Datasource
 import com.example.superheroes.model.Hero
 import com.example.superheroes.ui.theme.SuperHeroesTheme
@@ -32,7 +44,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             SuperHeroesTheme {
                 Surface() {
-                    SuperHeroesApp()
+                    SuperHeroesApp(
+                        modifier = Modifier.fillMaxSize()
+                    )
                 }
             }
         }
@@ -40,8 +54,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun SuperHeroesApp() {
+fun SuperHeroesApp(modifier: Modifier = Modifier) {
+    HeroesList(
+        Datasource.loadHeroes()
+    )
+}
 
+@Composable
+fun HeroesList(
+    herosList: List<Hero>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(items = herosList) { hero ->
+            HeroCard(
+                hero = hero,
+                modifier = Modifier
+                    .padding(vertical = 8.dp, horizontal = 16.dp)
+                    .fillMaxWidth()
+            )
+        }
+    }
 }
 
 @Composable
@@ -50,12 +85,32 @@ fun HeroCard(
     modifier: Modifier = Modifier
     ) {
     Card (
+        modifier = modifier
     ) {
-        Row {
-            Column() {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            modifier = Modifier.padding(16.dp)
+                .height(72.dp)
+                .fillMaxWidth()
+
+        ) {
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
                 Text(text = stringResource(id = hero.nameRes))
                 Text(text = stringResource(id = hero.descriptionRes))
             }
+
+            Image(
+                painter = painterResource(id = hero.imageRes),
+                contentDescription = stringResource(id = hero.nameRes),
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .padding(start = 16.dp)
+                    .size(72.dp)
+
+            )
         }
     }
 }
@@ -63,8 +118,8 @@ fun HeroCard(
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    SuperHeroesTheme {
-        //SuperHeroesApp()
-        HeroCard(Datasource.loadHeroes()[0])
+    SuperHeroesTheme(darkTheme = true) {
+        SuperHeroesApp()
+        //HeroCard(Datasource.loadHeroes()[0])
     }
 }
