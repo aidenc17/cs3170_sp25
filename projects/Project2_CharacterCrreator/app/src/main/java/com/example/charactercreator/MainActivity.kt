e1a75675befb347784add24a66a2021d8872bf81
James Anderson <james.anderson@wright.edu>
2025-03-25 18:38:47 -0400

    added my project 2 character creator solution

--- projects/Project2_CharacterCrreator/app/src/main/java/com/example/charactercreator/MainActivity.kt
+++ projects/Project2_CharacterCrreator/app/src/main/java/com/example/charactercreator/MainActivity.kt
@@ -1,32 +1,36 @@
 package com.example.charactercreator
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.twotone.KeyboardArrowDown
-import androidx.compose.material.icons.twotone.KeyboardArrowUp
-import androidx.compose.material.icons.twotone.ThumbUp
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.charactercreator.ui.theme.CharacterCreatorTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,7 +41,9 @@ class MainActivity : ComponentActivity() {
             CharacterCreatorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     CharacterCreatorApp(
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
@@ -47,68 +53,159 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CharacterCreatorApp(modifier: Modifier = Modifier) {
-    var staminaValue by remember { mutableStateOf(0) }
-    var strengthValue by remember { mutableStateOf(0) }
-
-    Row() {
-        StatButtons(
-            statName = "Stamina",
-            statValue = "$staminaValue",
-            plusClick = { staminaValue++ },
-            minusClick = { staminaValue-- }
-        )
-        Spacer(modifier = Modifier.padding(horizontal = 8.dp))
-        StatButtons(
-            statName = "Strength",
-            statValue = "$strengthValue",
-            plusClick = { strengthValue++ },
-            minusClick = { strengthValue-- }
-        )
+    val useMap = true
+    val MAX_STATS = 25
+    //var (statArray, setStats) = remember { mutableStateOf(Array<Int>(size = 4, init = { 0 })) }
+    var (statMap, setStatMap) = remember {
+        (mutableStateOf(
+            mutableMapOf(
+                "Stamina" to 0,
+                "Strength" to 0,
+                "Agility" to 0,
+                "Intellect" to 0
+            )
+        ))
     }
+    //var statList = mutableListOf(0, 0, 0, 0)
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+    ) {
+        Text(
+            text = "Character Creator", fontSize = 36.sp,
+            modifier = Modifier.padding(bottom = 64.dp)
+        )
+        Row(
+            horizontalArrangement = Arrangement.Start,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 64.dp)
+        ) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(2),
+                horizontalArrangement = Arrangement.spacedBy(0.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp)
+            ) {
+                items(statMap.keys.toList())
+                { stat ->
+                    StatButtons(
+                        statName = stat,
+                        value = statMap[stat].toString(),
+                        onPlusClick = {
+                            if (statMap.values.sum() < MAX_STATS) {
+                                setStatMap((statMap + (stat to statMap[stat]?.plus(1))) as HashMap<String, Int>)
+                            }
+                        },
+                        onMinusClick = {
+                            if (statMap.getOrDefault(stat, 0) > 0) {
+                                setStatMap((statMap + (stat to statMap[stat]?.minus(1))) as HashMap<String, Int>)
+                            }
+                        },
+                    )
+                }
+
+            }
+
+        }
+
+        Spacer(Modifier.padding(vertical = 8.dp))
+        Text(text = "Points remaining: ${MAX_STATS - statMap.values.sum()}/${MAX_STATS}")
+        Spacer(Modifier.padding(vertical = 8.dp))
+        AttributeList(stats = statMap)
+    }
+}
+
+@Composable
+fun AttributeList(
+    stats: Map<String, Int>,
+    modifier: Modifier = Modifier
+) {
+    val attributeMap = getAttributesFromStats(stats)
+    LazyColumn(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(horizontal = 64.dp)
+    ) {
+        items(attributeMap.keys.toList()) { attribute ->
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(text = "${attribute}:")
+                Text(text = "${attributeMap.getOrDefault(attribute, 0)}")
+            }
+        }
+
+    }
+}
+
+private fun getAttributesFromStats(stats: Map<String, Int>): Map<String, Int> {
+    return mapOf(
+        "Hit points" to
+            2 * stats.getOrDefault("Stamina", 0) +
+                    stats.getOrDefault("Strength", 0),
+        "Health regeneration" to
+            2 * stats.getOrDefault("Stamina", 0) +
+                    (stats.getOrDefault("Strength", 0) +
+                            stats.getOrDefault("Agility", 0)),
+        "Physical damage" to
+                2 * stats.getOrDefault("Strength", 0) +
+                    stats.getOrDefault("Agility", 0),
+        "Magic damage" to
+            2 * stats.getOrDefault("Agility", 0) +
+                    stats.getOrDefault("Intellect", 0)
+    )
 }
 
 @Composable
 fun StatButtons(
     statName: String,
-    statValue: String,
-    plusClick: () -> Unit,
-    minusClick: () -> Unit,
+    value: String,
+    onPlusClick: () -> Unit,
+    onMinusClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
 
     Column(
+        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        // add to stat
+    )
+    {
+        // increment stat
         Button(
-            onClick = plusClick
+            onClick = onPlusClick,
         ) {
             Icon(
-                imageVector = Icons.TwoTone.KeyboardArrowUp,
+                painter = painterResource(id = R.drawable.baseline_arrow_drop_up_24),
                 contentDescription = ""
             )
         }
+        Text(
+            text = statName
+        )
 
-        Text(text = statName)
-        Text(text = statValue)
-
-        // subtract from stat
-        Button(
-            onClick = minusClick
-        ) {
+        Text(
+            text = value
+        )
+        Button(onClick = onMinusClick) {
             Icon(
-                imageVector = Icons.TwoTone.KeyboardArrowDown,
+                painter = painterResource(id = R.drawable.baseline_arrow_drop_down_24),
                 contentDescription = ""
             )
         }
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CharacterCreatorPreview() {
     CharacterCreatorTheme {
-        CharacterCreatorApp(modifier = Modifier.fillMaxSize())
+        CharacterCreatorApp(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize()
+        )
     }
 }
